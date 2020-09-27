0390227342645d18c64cbcda314c1151bb98e23c
emelanson <elijahmelanson@gmail.com>
2020-09-27 19:55:32 -0400

    Wrote timeKeeper function for Timer.

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-//build the timer first
-var timer = document.getElementsByClassName("timer").value;
-console.log("timer", timer);
+//Timer
+var timer = document.querySelector(".timer");
+var timeLeft = 100;
 
-var time = setInterval(function () {}, 1000 * 100);
+//Here is function for timekeeping.  It will eventually subtract from timeLeft when wrong answers happen.
 
-console.log("time", time);
+function timeKeeper() {
+	var timeInterval = setInterval(function () {
+		timeLeft--;
+		timer.innerHTML = "TIMER: " + timeLeft;
+
+		if (timeLeft === 0) {
+			clearInterval(timeInterval);
+			//Function here to draw the end screen
+		}
+		console.log("timer", timeLeft);
+	}, 1000);
+}
+
+timeKeeper();
