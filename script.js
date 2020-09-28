b4dfae79e084a77fdb38ae06fdefadeeece5e544
emelanson <elijahmelanson@gmail.com>
2020-09-27 23:45:18 -0400

    Things got messy.  Should have committed sooner.  I was working on too many parts at the same time.
    I wanted to add some extra functionality to randomize the questions and the order they come in.  I tried implementing it while I was writing everything else and ran into issues about accessing objects and nested objects.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Timer
+//Timer vars
 var timer = document.querySelector(".timer");
 var timeLeft = 100;
 
@@ -17,4 +17,94 @@ function timeKeeper() {
 	}, 1000);
 }
 
-timeKeeper();
+//from stack overflow.  Used to sort our questions and answers randomly.
+function randomize(a, b) {
+	return Math.random() - 0.5;
+}
+
+// timeKeeper();
+
+//sample QUESTION.  I want to serve the answer bank and questions randomly as well.
+//Individual questions will be nested in a questionBank object.
+//ANSWERS will contain 3 false answers and a correct one.  Letters/display order will be assigned randomly.
+
+var sampQuestions = {
+	one: {
+		questionText:
+			"THIS IS THE TEXT FOR A QUESTION.  IT WILL BE A LITTLE LONGER I THINK.",
+		answerBank: [
+			{ correct: false, answer: "HTML" },
+			{ correct: false, answer: "CSS" },
+			{ correct: false, answer: "JS" },
+			{ correct: true, answer: "ReactJS" },
+		],
+	},
+
+	two: {
+		questionText:
+			"TH234234IS IS THE TEXT FOR A QUESTION. 234234 IT WILL BE A LITTLE LONGER I THINK.",
+		answerBank: [
+			{ correct: false, answer: "HTM23423L" },
+			{ correct: false, answer: "C34234SS" },
+			{ correct: false, answer: "J23423S" },
+			{ correct: true, answer: "Re234234actJS" },
+		],
+	},
+
+	//SLOW DOWN AND WRITE THIS IN STEPS.  FIRST RETURN A SINGLE ITEM.  THEN WE WILL WORRY ABOUT SPLICING OUT THE QUESTIONS SO WE DON'T RETURN THE SAME ONE TWICE.
+};
+
+function returnRandomQuestion() {
+	var questionObjIndex = [
+		Math.floor(Math.random() * Object.values(sampQuestions).length),
+	];
+	// Took a long time to figure this part out.  We can treat Object.entries as if it were an array and use [] AFTER, NOT INSIDE, the parenthesis to access it.
+	return Object.entries(sampQuestions)[questionObjIndex];
+}
+
+//functions to draw a question
+var mainDisplay = document.querySelector(".main-display");
+var subDisplay = document.querySelector(".sub-display");
+var answerButtonsDiv = document.querySelector(".answer-buttons-div");
+
+function createAnswerButton(a) {
+	let button = document.createElement("button", { type: "button" });
+
+	//set the text of the button, create an attribute we will check for the correct answer
+	button.textContent = a.answer;
+	button.setAttribute("correct", a.correct);
+
+	//for bootstrap
+	button.setAttribute("type", "button");
+	button.className = "btn btn-primary btn-lg w-25 mt-2";
+
+	console.log("answerbutton property:", button);
+
+	answerButtonsDiv.append(button);
+}
+
+function questionDisplay() {
+	//TODO: randomly pick the question.  Handle multiple questions.
+	var currentQuestion = returnRandomQuestion();
+	console.log("CURRENT QUESTION", currentQuestion);
+	mainDisplay.textContent = currentQuestion["questionText"];
+
+	//generate button array
+	//pass object entries into createAnswerButton()
+
+	//!!!!!!!!!!!!!!!! Rethink this !!!!!!!!!!!!!!!!!!//
+	// Object.entries(sampQuestion.answerBank).forEach(createAnswerButton());
+
+	//can I use my second technique to return the desired object?
+	for (var i = 0; i < 4; i++) {
+		console.log(
+			"Does this look something up",
+			Object.values(currentQuestion)[i]
+		);
+		createAnswerButton(currentQuestion[answerBank[i]]);
+	}
+}
+// console.log("RANDOM QUESTION", sampQuestions.returnRandomQuestion());
+
+console.log("RANDOM QUESTION", returnRandomQuestion());
+questionDisplay();
